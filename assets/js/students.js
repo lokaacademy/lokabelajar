6ed64d17d714e0cea407d88b7726cc68c5eafefc
Zainal Farid <zainal@offeo.com>
2023-07-10 16:54:09 +0800

    belajar javascript

---
+++ assets/js/students.js
@@ -0,0 +1,20 @@
+function loadStudents() {
+    const students = {
+        "Backend" : {
+            "name": "Nurman",
+            "role": "Backend",
+            "nilai": 80
+        },
+        "PM" : [{
+            "name": "Sartika Dewi",
+            "role": "PM",
+            "nilai": 90
+        },
+        {
+            "name": "Abu Sofyan",
+            "role": "PM",
+            "nilai": 90
+        }]
+    }
+    console.log('students ', students);
+}
